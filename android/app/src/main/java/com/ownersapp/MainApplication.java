3f9dc7728f858cb792f97368e8d5c5a5b113c07c
Amit Gupta <amithigh.gupta@gmail.com>
2019-05-24 16:39:17 +0530

    Added react-navigation

--- android/app/src/main/java/com/ownersapp/MainApplication.java
+++ android/app/src/main/java/com/ownersapp/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new RNGestureHandlerPackage(),
             new RNGeocoderPackage(), new MapsPackage());
         }
 
