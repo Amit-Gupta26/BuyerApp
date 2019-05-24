24586a40230808743a709d030bd0f90fd846e3a4
Amit Gupta <amithigh.gupta@gmail.com>
2019-05-24 16:21:39 +0530

    Added react-native-geocoder related changes.

--- android/app/src/main/java/com/ownersapp/MainApplication.java
+++ android/app/src/main/java/com/ownersapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactApplication;
+import com.devfd.RNGeocoder.RNGeocoderPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-            return Arrays.<ReactPackage>asList(new MainReactPackage(), new MapsPackage());
+            return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new RNGeocoderPackage(), new MapsPackage());
         }
 
         @Override
